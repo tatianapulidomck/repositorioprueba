de4c71169bbfae44a7da9e569e8ef535647a471d
Tatiana Pulido <tatiana_pulido@mckinsey.com>
2026-01-06 14:57:41 -0500

    primer commit prueba

---
+++ main.py
@@ -0,0 +1,6 @@
+number1 = float(input("Enter the first number")) 
+number2 = float(input("Enter the second number")) 
+
+result = number1 + number2
+
+print("The sum is:", result)
---
+++ prueba1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [2,4,5,4,5,7,8,9,10,12]
+
+def linear_regression(x,y):
+    x = np.array(x)
+    y = np.array(y)
+
+    m, b = np.polyfit(x,y,1)
+    return m,b
+
+print(linear_regression(x,y))
